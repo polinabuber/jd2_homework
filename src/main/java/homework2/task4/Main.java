cff8dc875c0ace4b7d1716d9e7381066c74604f9
Polina Buber <buberp0@gmail.com>
2023-10-22 17:11:16 +0300

    add task 4

--- src/main/java/homework2/task4/Main.java
+++ src/main/java/homework2/task4/Main.java
@@ -1,40 +1,63 @@
 package homework2.task4;
 
-
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static final String EXPENSES_INSERT = "INSERT INTO expenses (id,paydate,receiver,value) VALUES (?, ?, ?, ?)";
+    public static final String EXPENSES_FIND_ALL = "SELECT * FROM expenses";
+    public static final String RECEIVER_FIND_BY_ID = "SELECT name FROM receivers WHERE id = ";
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        if (args.length < 4) {
+            System.err.println("Please enter the values in the format  'id YYYY-MM-D receiverID price'");
+            return;
+        }
+        int id = Integer.parseInt(args[0]);
+        String paydate = args[1];
+        int receiver = Integer.parseInt(args[2]);
+        double value = Double.parseDouble(args[3]);
 
         try (Connection connection = ExpensesConnection.getConnection()) {
-            PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO expenses (id,paydate,receiver,value) VALUES (14, '2021-01-01', 1, 200)");
-            insertStatement.executeUpdate();
-
-            PreparedStatement selectStatement = connection.prepareStatement("SELECT * FROM expenses");
-            ResultSet resultSet = selectStatement.executeQuery();
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                Date paydate = resultSet.getDate("paydate");
-                int receiverId = resultSet.getInt("receiver");
-                double value = resultSet.getDouble("value");
-
-                PreparedStatement receiverStatement = connection.prepareStatement("SELECT name FROM receivers WHERE id = " + receiverId);
-                ResultSet receiverResultSet = receiverStatement.executeQuery();
-                String receiverName = "";
-                if (receiverResultSet.next()) {
-                    receiverName = receiverResultSet.getString("name");
-                }
-
-                System.out.println(id + " " + paydate + " " + receiverName + " " + value);
-            }
-
+            insertExpense(id, paydate, receiver, value, connection);
+            printAllExpenses(connection);
         } catch (SQLException e) {
-            System.err.println("An error occurred while interacting with the database: " + e.getMessage());
-        } catch (ClassNotFoundException e) {
-            System.err.println("The JDBC driver was not found: " + e.getMessage());
-        } catch (Exception e) {
-            System.err.println("An unexpected error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void printAllExpenses(Connection connection) throws SQLException {
+        PreparedStatement selectStatement = connection.prepareStatement(EXPENSES_FIND_ALL);
+        ResultSet resultSet = selectStatement.executeQuery();
+        while (resultSet.next()) {
+            int expenseId = resultSet.getInt("id");
+            Date expensePaydate = resultSet.getDate("paydate");
+            int expenseReceiverId = resultSet.getInt("receiver");
+            double expenseValue = resultSet.getDouble("value");
+            String receiverName = getReceiverNameById(expenseReceiverId, connection);
+            System.out.println(expenseId + " " + expensePaydate + " " + receiverName + " " + expenseValue);
         }
     }
+
+    private static String getReceiverNameById(int id, Connection connection) throws SQLException {
+        PreparedStatement receiverStatement = connection.prepareStatement(RECEIVER_FIND_BY_ID + id);
+        ResultSet receiverResultSet = receiverStatement.executeQuery();
+        String receiverName = "";
+        if (receiverResultSet.next()) {
+            receiverName = receiverResultSet.getString("name");
+        }
+        return receiverName;
+    }
+
+    private static void insertExpense(int id, String paydate, int receiver, double value, Connection connection) throws SQLException {
+        PreparedStatement insertStatement = connection.prepareStatement(EXPENSES_INSERT);
+        insertStatement.setInt(1, id);
+        insertStatement.setDate(2, Date.valueOf(paydate));
+        insertStatement.setInt(3, receiver);
+        insertStatement.setDouble(4, value);
+        insertStatement.executeUpdate();
+    }
+
+
 }
