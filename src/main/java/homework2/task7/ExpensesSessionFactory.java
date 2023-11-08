c8b880158eae0ee750e7b063a4791c2df24f2a3a
Polina Buber <buberp0@gmail.com>
2023-11-08 20:28:16 +0300

    add sessionfactory classes

---
+++ src/main/java/homework2/task7/ExpensesSessionFactory.java
@@ -0,0 +1,21 @@
+package homework2.task7;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+public class ExpensesSessionFactory {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                sessionFactory = new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
---
+++ src/test/java/homework2/TestSessionFactory.java
@@ -0,0 +1,21 @@
+package homework2;
+
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class TestSessionFactory {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                sessionFactory = new Configuration()
+                        .configure("test.hibernate.cfg.xml")
+                        .buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
