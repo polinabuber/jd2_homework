7e31d4bae5c6e8e2802328b444a88103db203a55
Polina Buber <buberp0@gmail.com>
2023-11-24 22:18:36 +0300

    add implementation for the CRUD operations and add a getIdentifier method to extract identifiers from Persistent

--- src/main/java/homework2/task7/dao/DaoImpl.java
+++ src/main/java/homework2/task7/dao/DaoImpl.java
@@ -4,12 +4,15 @@ import homework2.task7.pojo.*;
 import org.hibernate.*;
 import org.hibernate.query.Query;
 
+import java.io.*;
 import java.util.*;
 
 public class DaoImpl implements Dao {
 
     public static final String EXPENSES_ALL = "FROM Expenses";
     public static final String RECEIVERS_ALL = "FROM Receiver";
+    public static final String CLIENTS_ALL = "FROM Client";
+    public static final String CLIENT_DETAILS_ALL = "FROM ClientDetails";
     private final SessionFactory sessionFactory;
 
 
@@ -19,6 +22,29 @@ public class DaoImpl implements Dao {
         }
         this.sessionFactory = sessionFactory;
     }
+    public Session getSession() {
+        return this.sessionFactory.getCurrentSession();
+    }
+    public Serializable getIdentifier(Object entity) {
+        if (entity == null) {
+            return null;
+        }
+        Transaction transaction = null;
+        try {
+            transaction = getSession().beginTransaction();
+            getSession().saveOrUpdate(entity);
+            Serializable id = getSession().getIdentifier(entity);
+            transaction.commit();
+            return id;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+
     public void flushSession() {
         Transaction transaction = null;
         try (Session session = sessionFactory.getCurrentSession()) {
@@ -32,8 +58,6 @@ public class DaoImpl implements Dao {
             System.out.println("Error when flushing session: " + e.getMessage());
         }
     }
-
-
     public void clearSession() {
         Transaction transaction = null;
         try (Session session = sessionFactory.getCurrentSession()) {
@@ -45,36 +69,6 @@ public class DaoImpl implements Dao {
                 transaction.rollback();
             }
             System.out.println("Error when clearing session: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void updateExpense(Expenses expenses) {
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            session.update(expenses);
-            transaction.commit();
-        } catch (RuntimeException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            System.out.println("Error when updating expense: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void updateReceiver(Receiver receiver) {
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            session.update(receiver);
-            transaction.commit();
-        } catch (RuntimeException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            System.out.println("Error when updating receiver: " + e.getMessage());
         }
     }
     @Override
@@ -102,8 +96,6 @@ public class DaoImpl implements Dao {
         }
     }
 
-
-
     @Override
     public ArrayList<Expenses> getExpenses() {
         try (Session session = sessionFactory.openSession()) {
@@ -121,27 +113,6 @@ public class DaoImpl implements Dao {
             return session.get(Expenses.class, num);
         } catch (HibernateException e) {
             System.out.println("Error when receiving expense: " + e.getMessage());
-            return null;
-        }
-    }
-
-    @Override
-    public ArrayList<Receiver> getReceivers() {
-        try (Session session = sessionFactory.openSession()) {
-            Query<Receiver> query = session.createQuery(RECEIVERS_ALL, Receiver.class);
-            return new ArrayList<>(query.list());
-        } catch (HibernateException e) {
-            System.out.println("Error getting list of receivers: " + e.getMessage());
-            return null;
-        }
-    }
-
-    @Override
-    public Receiver getReceiver(int num) {
-        try (Session session = sessionFactory.openSession()) {
-            return session.get(Receiver.class, num);
-        } catch (HibernateException e) {
-            System.out.println("Error when receiving receiver: " + e.getMessage());
             return null;
         }
     }
@@ -163,23 +134,19 @@ public class DaoImpl implements Dao {
     }
 
     @Override
-    public int addReceiver(Receiver receiver) {
+    public void updateExpense(Expenses expenses) {
         Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            int id = (int) session.save(receiver);
+            session.update(expenses);
             transaction.commit();
-            return id;
         } catch (RuntimeException e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            System.out.println("Error when adding receiver: " + e.getMessage());
-            return -1;
+            System.out.println("Error when updating expense: " + e.getMessage());
         }
     }
-
-
     @Override
     public boolean deleteExpense(Expenses expenses) {
         Transaction transaction = null;
@@ -196,7 +163,57 @@ public class DaoImpl implements Dao {
             return false;
         }
     }
+    @Override
+    public ArrayList<Receiver> getReceivers() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Receiver> query = session.createQuery(RECEIVERS_ALL, Receiver.class);
+            return new ArrayList<>(query.list());
+        } catch (HibernateException e) {
+            System.out.println("Error getting list of receivers: " + e.getMessage());
+            return null;
+        }
+    }
 
+    @Override
+    public Receiver getReceiver(int num) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Receiver.class, num);
+        } catch (HibernateException e) {
+            System.out.println("Error when receiving receiver: " + e.getMessage());
+            return null;
+        }
+    }
+    @Override
+    public int addReceiver(Receiver receiver) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            int id = (int) session.save(receiver);
+            transaction.commit();
+            return id;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Error when adding receiver: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    @Override
+    public void updateReceiver(Receiver receiver) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.update(receiver);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Error when updating receiver: " + e.getMessage());
+        }
+    }
     @Override
     public boolean deleteReceiver(Receiver receiver) {
         Transaction transaction = null;
@@ -213,7 +230,135 @@ public class DaoImpl implements Dao {
             return false;
         }
     }
+    @Override
+    public ArrayList<Client> getClients() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Client> query = session.createQuery(CLIENTS_ALL, Client.class);
+            return new ArrayList<>(query.list());
+        } catch (HibernateException e) {
+            System.out.println("Error when getting list of clients: " + e.getMessage());
+            return null;
+        }
+    }
+    @Override
+    public Client getClient(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Client.class, id);
+        }
+    }
+
+    @Override
+    public int addClient(Client client) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            int id = (int) session.save(client);
+            transaction.commit();
+            return id;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Error when adding client:" + e.getMessage());
+            return -1;
+        }
+    }
+    @Override
+    public void updateClient(Client client) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.update(client);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Error when updating client: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean deleteClient(Client client) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(client);
+            transaction.commit();
+            return true;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Error when deleting client: " + e.getMessage());
+            return false;
+        }
+    }
 
 
 
+    @Override
+    public ArrayList<ClientDetails> getClientDetails() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<ClientDetails> query = session.createQuery(CLIENT_DETAILS_ALL, ClientDetails.class);
+            return new ArrayList<>(query.list());
+        } catch (HibernateException e) {
+            System.out.println("Error when getting list of client details: " + e.getMessage());
+            return null;
+        }
+    }
+    @Override
+    public ClientDetails getClientDetails(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(ClientDetails.class, id);
+        }
+    }
+
+    @Override
+    public int addClientDetails(ClientDetails clientDetails) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            int id = (int) session.save(clientDetails);
+            transaction.commit();
+            return id;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Error when adding client details:" + e.getMessage());
+            return -1;
+        }
+    }
+    @Override
+    public void updateClientDetails(ClientDetails clientDetails) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.update(clientDetails);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Error when updating client details: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean deleteClientDetails(ClientDetails clientDetails) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(clientDetails);
+            transaction.commit();
+            return true;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Error when deleting client details: " + e.getMessage());
+            return false;
+        }
+    }
 }
